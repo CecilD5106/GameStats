b2ec754d2749f5f8fc6bc2fa1f195ec9b4e00db8
Cecil Davis <cecild5106@hotmail.com>
2019-09-14 22:38:28 -0500

    Modified usings statements

--- Program.cs
+++ Program.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Use team statistics to create game statistics
 using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
 
 namespace GameStats
 {
